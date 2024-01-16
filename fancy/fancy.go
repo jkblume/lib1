6777a5d22cb39d7e72ab55876f74679f76555e65
Jakob Blume <jakob.blume@mailbox.org>
2024-01-16 16:03:26 +0100

    add fancy package

---
+++ fancy/fancy.go
@@ -0,0 +1,5 @@
+package fancy
+
+func FancyStuff() {
+	println("fancy")
+}
--- foo.go
+++ foo.go
@@ -1,7 +1,14 @@
 package lib1
 
-import "github.com/jkblume/lib2"
+import (
+	"fmt"
+	"github.com/jkblume/lib2"
+)
 
 func Foo() {
 	lib2.Bar()
+}
+
+func Foo2() {
+	fmt.Println("foo2")
 }
